e48968f729b533a54494133593cf3ef6d56a36de
kyoyasai <ktakuku14@gmail.com>
2020-11-21 17:46:03 +0900

    テーブルカラム名編集(day_id)

--- db/migrate/20201121035853_create_items.rb
+++ db/migrate/20201121035853_create_items.rb
@@ -5,9 +5,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text       :description,   null: false
       t.integer    :category_id,   null: false
       t.integer    :condition_id,  null: false
-      t.integer    :fee_id,       null: false
+      t.integer    :fee_id,        null: false
       t.integer    :prefecture_id, null: false
-      t.integer    :days_id,       null: false
+      t.integer    :day_id,        null: false
       t.integer    :price,         null: false
       t.references :user,          foreign_key: true
       t.timestamps
