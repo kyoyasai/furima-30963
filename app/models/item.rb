7d466df832a3f68078189a0d645b86926b18ae75
kyoyasai <ktakuku14@gmail.com>
2020-11-22 22:17:51 +0900

    バリデーションの修正(Item)

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,16 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :fee_id
-    validates :prefecture_id
-    validates :day_id
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+    validates_format_of :price, with: /\A\d{3,}+\z/, message: 'Half-width number'
+
+    with_options numericality: { other_than: 1, message: "Select" } do
+      validates :category_id
+      validates :condition_id
+      validates :fee_id
+      validates :prefecture_id
+      validates :day_id
+    end
   end
 
 end
