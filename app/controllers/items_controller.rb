e1fba2263f12f22a3ce98ad79851a777c23d3064
kyoyasai <ktakuku14@gmail.com>
2020-11-25 22:58:09 +0900

    コントローラー設定(商品情報編集機能)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :user_judge, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,9 +23,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :fee_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def user_judge
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
